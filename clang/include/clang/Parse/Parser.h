b7df4eacbefb3cb5d9df2347c9b3a0c4670aa07d
Henrik G. Olsson <h_olsson@apple.com>
2025-03-10 11:25:36 -0700

    [BoundsSafety][APINotes] Add support for bounds attribute on return value
    Previously you could only attach bounds attributes to function
    parameters. Now return values are also supported, although not yet for
    ObjectiveC methods. Adds test case for bounds safety API notes in C++,
    and fixes bug where late parsed bounds attributes on C++ method
    parameters are parsed twice, due to not being removed from the late
    attributes list after parsing.
    
    rdar://146332875

--- clang/include/clang/Parse/Parser.h
+++ clang/include/clang/Parse/Parser.h
@@ -1655,6 +1655,9 @@ private:
                                 ParsedAttributes *OutAttrs = nullptr);
   void ParseLexedAttribute(LateParsedAttribute &LA,
                            bool EnterScope, bool OnDefinition);
+  /// Note: it's important the the LateParsedAttribute is removed after parsing
+  /// to prevent being reparsed later in the wrong context. This is handled
+  /// automatically by Parser::ParseLexedCAttributeList.
   void ParseLexedCAttribute(LateParsedAttribute &LA, bool EnterScope,
                             ParsedAttributes *OutAttrs = nullptr);
   void ParseLexedMethodDeclarations(ParsingClass &Class);
@@ -3944,10 +3947,11 @@ private:
   /// \param ParentDecl If a function or method is provided, the parameters are
   ///   added to the current parsing context.
   /// \param IncludeLoc The location at which this parse was triggered.
-  ExprResult ParseBoundsAttributeArgFromString(StringRef ExprStr,
-                                               StringRef Context,
-                                               Decl *ParentDecl,
-                                               SourceLocation IncludeLoc);
+  /// \param Payload Info to be passed along to the completion handler before
+  /// leaving the current context
+  void ParseBoundsAttributeArgFromString(
+      StringRef ExprStr, StringRef Context, Decl *ParentDecl,
+      SourceLocation IncludeLoc, Sema::IncompleteBoundsAttributeInfo Payload);
   /* TO_UPSTREAM(BoundsSafety) OFF */
 
   //===--------------------------------------------------------------------===//

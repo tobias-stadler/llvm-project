d4162949d2fcfb868b6bedf941021ed2027c12ad
Guy David <49722543+guy-david@users.noreply.github.com>
2025-03-12 17:17:51 +0200

    AddressSanitizer: Add use-after-scope to pass options (#130924)

--- llvm/lib/Transforms/Instrumentation/AddressSanitizer.cpp
+++ llvm/lib/Transforms/Instrumentation/AddressSanitizer.cpp
@@ -1237,7 +1237,9 @@ void AddressSanitizerPass::printPipeline(
       OS, MapClassName2PassName);
   OS << '<';
   if (Options.CompileKernel)
-    OS << "kernel";
+    OS << "kernel;";
+  if (Options.UseAfterScope)
+    OS << "use-after-scope";
   OS << '>';
 }
 
